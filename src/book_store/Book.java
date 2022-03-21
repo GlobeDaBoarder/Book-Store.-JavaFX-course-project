9870f2415280dc2373b0dc83967a241f49f0fe67
Gleb Ivashyn <gleb.ivashyn@stud.vgtu.lt>
2022-03-21 22:44:31 +0200

    DB initial

--- src/book_store/Book.java
+++ src/book_store/Book.java
@@ -1,30 +1,30 @@
 package book_store;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDate;
-
-@Data
 
 public class Book {
     private int productID;
     private double price;
     private String name;
     private String author;
+    private String description;
     private LocalDate releaseDate;
     private int pageCount;
     private eBookLang lang;
     private int quantityAvalible;
     private eBookGenre genre;
 
-
-    public Book(int productID, double price, String name, String author,
-                LocalDate releaseDate, int pageCount, eBookLang lang,
-                int quantityAvalible, eBookGenre genre) {
+    public Book(int productID, double price, String name, String author, String description, LocalDate releaseDate,
+                int pageCount, eBookLang lang, int quantityAvalible, eBookGenre genre) {
         this.productID = productID;
         this.price = price;
         this.name = name;
         this.author = author;
+        this.description = description;
         this.releaseDate = releaseDate;
         this.pageCount = pageCount;
         this.lang = lang;
@@ -32,13 +32,12 @@ public class Book {
         this.genre = genre;
     }
 
-    public Book(double price, String name, String author,
-                LocalDate releaseDate, int pageCount, eBookLang lang,
-                int quantityAvalible, eBookGenre genre) {
-        this.productID = productID;
+    public Book(double price, String name, String author, String description, LocalDate releaseDate, int pageCount,
+                eBookLang lang, int quantityAvalible, eBookGenre genre) {
         this.price = price;
         this.name = name;
         this.author = author;
+        this.description = description;
         this.releaseDate = releaseDate;
         this.pageCount = pageCount;
         this.lang = lang;
@@ -53,11 +52,92 @@ public class Book {
                 ", price=" + price +
                 ", name='" + name + '\'' +
                 ", author='" + author + '\'' +
+                ", description='" + description + '\'' +
                 ", releaseDate=" + releaseDate +
                 ", pageCount=" + pageCount +
                 ", lang=" + lang +
                 ", quantityAvalible=" + quantityAvalible +
                 ", genre=" + genre +
                 '}';
+    }
+
+    public int getProductID() {
+        return productID;
+    }
+
+    public void setProductID(int productID) {
+        this.productID = productID;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(LocalDate releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public int getPageCount() {
+        return pageCount;
+    }
+
+    public void setPageCount(int pageCount) {
+        this.pageCount = pageCount;
+    }
+
+    public eBookLang getLang() {
+        return lang;
+    }
+
+    public void setLang(eBookLang lang) {
+        this.lang = lang;
+    }
+
+    public int getQuantityAvalible() {
+        return quantityAvalible;
+    }
+
+    public void setQuantityAvalible(int quantityAvalible) {
+        this.quantityAvalible = quantityAvalible;
+    }
+
+    public eBookGenre getGenre() {
+        return genre;
+    }
+
+    public void setGenre(eBookGenre genre) {
+        this.genre = genre;
     }
 }
--- src/controllers/MainWindow.java
+++ src/controllers/MainWindow.java
@@ -5,9 +5,19 @@ import book_store.eBookGenre;
 import book_store.eBookLang;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import utils.DBoperations;
 
-public class MainWindow {
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+public class MainWindow implements Initializable {
     @FXML
     public ListView bookList;
     @FXML
@@ -17,15 +27,47 @@ public class MainWindow {
     public ComboBox bookGenre;
     public DatePicker publDate;
     public TextField pgNum;
-    public TextField bookEdition;
     public TextField bookAuthor;
     public TextField bookPrice;
     public TextField bookQuantity;
+    public ListView bookListMngr;
 
-    public void addBook(ActionEvent actionEvent) {
+    public void addBook(ActionEvent actionEvent) throws SQLException {
         Book book = new Book(Double.parseDouble(bookPrice.getText()), bookTitle.getText(),
-                bookAuthor.getText(), publDate.getValue(), Integer.parseInt(pgNum.getText()), eBookLang.English,
+                bookAuthor.getText(), bookDescription.getText(), publDate.getValue(), Integer.parseInt(pgNum.getText()), eBookLang.English,
                 Integer.parseInt(bookQuantity.getText()), eBookGenre.SCI_FI);
         System.out.println(book);
+
+        Connection connection = DBoperations.connectToDB();
+        String insertQuery = "INSERT INTO books(`price`, `name`, `author`, `description`, `releaseDate`, `pageCount`, `lang`,`quantityAvalible`, `genre`) VALUES(?,?,?,?,?,?,?,?,?)";
+        PreparedStatement preparedStatement = connection.prepareStatement(insertQuery);
+        preparedStatement.setDouble(1, book.getPrice());
+        preparedStatement.setString(2, book.getName());
+        preparedStatement.setString(3, book.getAuthor());
+        preparedStatement.setString(4, book.getDescription());
+        preparedStatement.setDate(5, Date.valueOf(book.getReleaseDate()));
+        preparedStatement.setInt(6, book.getPageCount());
+        preparedStatement.setString(7, String.valueOf(book.getLang()));
+        preparedStatement.setInt(8, book.getQuantityAvalible());
+        preparedStatement.setString(9, String.valueOf(book.getGenre()));
+        preparedStatement.execute();
+        DBoperations.disconnectFromDB(connection, preparedStatement);
+        //Insert Query
+        //run query
+        //close connection
+        refreshTable();
+    }
+
+    private void refreshTable() {
+        bookList.getItems().clear();
+        bookListMngr.getItems().clear();
+        ArrayList<Book> books = DBoperations.getAllBooksFromDb();
+        books.forEach(b -> bookList.getItems().add(b));
+        books.forEach(b -> bookListMngr.getItems().add(b));
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        refreshTable();
     }
 }
