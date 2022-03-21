5fde9d529168b0d700bedfcd178b3beace5a2423
Gleb Ivashyn <gleb.ivashyn@stud.vgtu.lt>
2022-03-21 16:55:58 +0200

    basics of login

---
+++ src/controllers/MainWindow.java
@@ -0,0 +1,12 @@
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ListView;
+
+public class MainWindow {
+    @FXML
+    public ListView bookList;
+    @FXML
+    public CheckBox check;
+}
--- src/controllers/StartController.java
+++ src/controllers/StartController.java
@@ -1,13 +1,22 @@
 package controllers;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class StartController extends Application {
+    @FXML
+    public TextField loginF;
+    @FXML
+    public PasswordField pswF;
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(StartController.class.getResource("../view/Start.fxml"));
@@ -19,5 +28,12 @@ public class StartController extends Application {
 
     public static void main(String[] args) {
         launch();
+    }
+
+    public void logIn(ActionEvent actionEvent) {
+        System.out.println(loginF.getText() + ' ' + pswF.getText());
+    }
+
+    public void SignUp(ActionEvent actionEvent) {
     }
 }
